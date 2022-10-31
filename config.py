31e025c392f82efafb0d8fef55268cb262561c84
redixyz <117117297+redixyz@users.noreply.github.com>
2022-11-01 00:22:15 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 
 #ТОКЕНЫ ДЛЯ БОТА В ТГ. ЗАСОВЫВАЙТЕ ТОКЕН В "MAIN_BOT_TOKEN"
 #USE "MAIN_BOT_TOKEN"
-MAIN_BOT_TOKEN = "5536244524:AAEK4grS9Yk3FoVpJlSg47c_MU85Mg_1Cvo"
-TEST_BOT_TOKEN = "5119379547:AAE0da3vqgW7tbxKYJedECJ1dsxTWpeQ5bU"
+MAIN_BOT_TOKEN = ""
+TEST_BOT_TOKEN = ""
 
 #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 #Для старта напишите в бота "/r"
